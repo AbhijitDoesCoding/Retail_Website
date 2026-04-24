3204213e398609c0fb6cf0d20a05ceb93b2e319d
AbhijitDoesCoding <abalpande1804@gmail.com>
2026-04-24 12:29:35 +0530

    link

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    SUPABASE_URL: 'https://ionszphvxhffqfwlohiv.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvbnN6cGh2eGhmZnFmd2xvaGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMTMyNzQsImV4cCI6MjA5MTg4OTI3NH0.KYYW_eEJIBJQB1-7fvxUo7N4GCxN9PzpROZQoef0xh0'
+};
